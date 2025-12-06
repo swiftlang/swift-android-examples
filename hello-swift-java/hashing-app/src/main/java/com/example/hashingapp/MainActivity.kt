602c8e1a1a8d0671eaf635bc2f7aab3a28f79923
Mads Odgaard <mads.odgaard@frameo.com>
2025-12-06 17:10:28 +0100

    typo

--- hello-swift-java/hashing-app/src/main/java/com/example/hashingapp/MainActivity.kt
+++ hello-swift-java/hashing-app/src/main/java/com/example/hashingapp/MainActivity.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.hashingapp.ui.theme.HashingAppTheme
-import com.example.swifthashing.SwiftHashingf
+import com.example.swifthashing.SwiftHashing
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
